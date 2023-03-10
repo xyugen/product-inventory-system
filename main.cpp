6f7a41bf9d06c4af77b5c723f24ae7523f989ea9
Alex Arias <alex.arias.jorym@gmail.com>
2023-03-10 22:32:00 +0800

    feat: create user responsive menu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 /*
  * PRODUCT INVENTORY SYSTEM
  * CREATOR: XYUGEN
  * LICENCE: MIT
  */
 
+#define PFNAME "products.txt"
+#define PTFNAME "products.temp.txt"
+#define UFNAME "users.txt"
+#define UTFNAME "users.temp.txt"
 #define MAX_LEN 100
 
 typedef struct {
@@ -20,8 +25,31 @@ typedef struct {
     bool is_admin;
 } User;
 
-int main() {
+User current_user;
 
+void menu ()
+{
+    char last[MAX_LEN] = "\n";
+    if (current_user.is_admin) {
+        strcpy(last, "[9] Add a user\n\n");
+    }
+    printf("\t\t\tProduct Inventory System\n"
+           "----------------------------------------\n"
+           "[1] Add a product\n"
+           "[2] See product list\n"
+           "[3] Update a product\n"
+           "[4] Remove a product\n"
+           "%s"
+           "[0] Exit\n"
+           "----------------------------------------\n", last);
+}
+
+int main() {
+    int c;
+
+    do {
+        menu();
+    } while (c != 0);
 
     return 0;
 }
