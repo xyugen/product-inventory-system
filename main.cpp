55ebea85acfab743e10a0e543258ddf2f028d488
Alex Arias <alex.arias.jorym@gmail.com>
2023-03-10 22:20:15 +0800

    feat: create Product and User structure

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@
  * LICENCE: MIT
  */
 
+#define MAX_LEN 100
+
+typedef struct {
+    int id;
+    char name[MAX_LEN];
+    int quantity;
+    float price;
+} Product;
+
+typedef struct {
+    int id;
+    char username[MAX_LEN], password[MAX_LEN];
+    bool is_admin;
+} User;
+
 int main() {
 
 
