1ea9343369208d09e93d2120c7d1710cc8a39e43
Alex Arias <alex.arias.jorym@gmail.com>
2023-03-11 00:31:03 +0800

    fix:admin-ability to create new users

--- main.cpp
+++ main.cpp
@@ -82,6 +82,46 @@ bool checkUserID(int id)
     return false;
 }
 
+bool checkUsername(char* username)
+{
+    FILE* fp = openFile(UFNAME, "r");
+    if (fp == NULL)
+    {
+        return false;
+    }
+
+    User user;
+    while (fread(&user, sizeof(user), 1, fp))
+    {
+        if (!strcmp(username, user.username))
+        {
+            fclose(fp);
+            return true;
+        }
+    }
+
+    fclose(fp);
+    return false;
+}
+
+int latestUserID()
+{
+    FILE* fp = openFile(UFNAME, "r");
+
+    User user;
+    int lastID;
+    while (fread(&user, sizeof(user), 1, fp))
+    {
+        if (user.id > lastID)
+        {
+            lastID = user.id;
+        }
+    }
+
+    fclose(fp);
+    return lastID;
+}
+
 // Removes any new line
 void rmNewline(char* input)
 {
@@ -147,6 +187,7 @@ void login()
         if (is_user)
         {
             is_logged = true;
+            current_user.is_admin = user.is_admin;
         }
         else
         {
@@ -157,10 +198,62 @@ void login()
     fclose(fp);
 }
 
+void adduser()
+{
+    FILE* fp = openFile(UFNAME, "a");
+
+    User newUser;
+    printf("NEW USER\n");
+
+    printf("Enter username: ");
+    getchar();
+    fgets(newUser.username, MAX_LEN, stdin);
+    rmNewline(newUser.username);
+
+    // Check if username already exists
+    if (checkUsername(newUser.username))
+    {
+        printf("Username already exists!");
+        return;
+    }
+
+    printf("Enter password: ");
+    fgets(newUser.password, MAX_LEN, stdin);
+    rmNewline(newUser.password);
+
+    int c;
+    printf("Is an admin?\n"
+           "[1] Yes\n"
+           "[0] No (default)\n"
+           ">> ");
+    scanf("%d", &c);
+
+    switch (c) {
+        case 1:
+            newUser.is_admin = true;
+            break;
+        case 2:
+        default:
+            newUser.is_admin = false;
+            break;
+    }
+
+    newUser.id = latestUserID() + 1;
+
+    fwrite(&newUser, sizeof(newUser), 1, fp);
+
+    printf("New user created successfully:\n"
+           "User ID: %d\n"
+           "Username: %s\n"
+           "Password: %s\n"
+           "Admin? %d", newUser.id, newUser.username, newUser.password, newUser.is_admin);
+
+    fclose(fp);
+}
+
 void menu ()
 {
     char last[MAX_LEN] = "\n";
-    printf("%d", current_user.is_admin);
     if (current_user.is_admin) {
         strcpy(last, "[9] Add a user\n\n");
     }
@@ -200,7 +293,7 @@ int main() {
                 break;
             case 9:
                 if (current_user.is_admin) {
-
+                    adduser();
                 } else printf("Invalid input...\n");
                 break;
             case 0:
